b079c0a5cc6d04057d746d676c3b7fad8aa767b3
LukutusHD <theyodacraft@gmail.com>
2017-04-19 16:58:56 +0200

    * added Option Window and removed old scrap

--- assets/js/MainWindow.js
+++ assets/js/MainWindow.js
@@ -3,11 +3,13 @@ const path = require("path");
 const url = require("url");
 
 let button = document.getElementById("login_window");
+let option_window = document.getElementById("options");
 let loginWindow = null;
+let optionWindow = null;
 
 button.onclick = function createWindow() {
     loginWindow = new BrowserWindow({
-        "height": 460,
+        "height": 200,
         "width": 290,
         "title": "Login",
         "menu": null,
@@ -25,5 +27,28 @@ button.onclick = function createWindow() {
 
     loginWindow.on('closed', () => {
         loginWindow = null;
+    });
+}
+
+option_window.onclick = function createWindow() {
+    optionWindow = new BrowserWindow({
+        "height": 460,
+        "width": 500,
+        "title": "Option",
+        "menu": null,
+        "icon": "./gui/icon.png",
+        "frame": false,
+    });
+
+    optionWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'OptionWindow.html'),
+        protocol: 'file:',
+        slashes: true,
+    }));
+
+    optionWindow.webContents.openDevTools();
+
+    optionWindow.on('closed', () => {
+        optionWindow = null;
     });
 }
