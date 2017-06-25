624cf0fbb49635fca5c92d8ff251a8eef6d6cc20
LukutusHD <theyodacraft@gmail.com>
2017-06-25 21:52:46 +0200

    * added login Window

--- assets/js/MainWindow.js
+++ assets/js/MainWindow.js
@@ -3,11 +3,7 @@ const path = require("path");
 const url = require("url");
 
 let button = document.getElementById("login");
-let option_window = document.getElementById("options");
-let login_1 = 0;
-let option_1 = null;
 let loginWindow = null;
-let optionWindow = null;
 
 button.onclick = function createWindow() {
     loginWindow = new BrowserWindow({
@@ -29,29 +25,5 @@ button.onclick = function createWindow() {
 
     loginWindow.on('closed', () => {
         loginWindow = null;
-    }
-}
-
-option_window.onclick = function createWindow() {
-    if option_1
-    optionWindow = new BrowserWindow({
-        "height": 460,
-        "width": 500,
-        "title": "Option",
-        "menu": null,
-        "icon": "./gui/icon.png",
-        "frame": false,
-    });
-
-    optionWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'OptionWindow.html'),
-        protocol: 'file:',
-        slashes: true,
-    }));
-
-    optionWindow.webContents.openDevTools();
-
-    optionWindow.on('closed', () => {
-        optionWindow = null;
     });
 }
